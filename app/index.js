d460bd130b6b6c8d620e907ca9c1290cfdde67b0
benji7425 <benji7425@outlook.com>
2017-06-03 23:22:34 +0100

    Add check against threshold to assign or remove the active role

--- app/index.js
+++ app/index.js
@@ -4,6 +4,7 @@ var fs = require("fs");
 
 //external lib imports
 var JsonFile = require("jsonfile");
+var DateDiff = require("date-diff");
 
 var users = {};
 var config;
@@ -17,6 +18,7 @@ module.exports = (_config) => {
 		Console.log(users);
 
 		writeToFile();
+		checkUsersAgainstThreshold(bot);
 	};
 
 	this.onMessage = (bot, user, userID, channelID, message) => {
@@ -29,7 +31,34 @@ module.exports = (_config) => {
 
 var writeToFile = () => {
 	JsonFile.writeFile(config.saveFile, users, (err) => { if (err) Console.error(err); });
-	// let saveIntervalMs = config.saveIntervalMins * 60 * 1000;
-	let saveIntervalMs = 1000;
+	let saveIntervalMs = config.saveIntervalMins * 60 * 1000;	
 	setTimeout(writeToFile, saveIntervalMs);
+};
+
+var checkUsersAgainstThreshold = (bot, doSetTimeout = true) => {
+	var now = new Date(); //get current date
+	var inactiveThresholdDays = parseInt(config.inactiveThresholdDays); //get an integer for the number of days a user must have been inactive for before the role is removed
+	Object.keys(users).forEach(userID => { //iterate over the user IDs
+		var diff = new DateDiff(now, users[userID]); //calculate the difference between the current date and the last time the user was active
+
+		//remove the "active" role from the user if they haven't been active within the threshold, give them it if they have
+		if (diff.days() > inactiveThresholdDays)
+			bot.removeFromRole({
+				serverID: config.serverID,
+				userID: userID,
+				roleID: config.activeRoleID
+			}, (err, response) => { if (err) Console.error(err, response); });
+		else
+			bot.addToRole({
+				serverID: config.serverID,
+				userID: userID,
+				roleID: config.activeRoleID
+			}, (err, response) => { if (err) Console.error(err, response); });
+	});
+
+	//set the timeout to wait before this function should recur
+	if (doSetTimeout) {
+		var waitMs = parseInt(config.checkActivityIntervalDays) * 24 * 60 * 60 * 1000;
+		setTimeout(() => { checkUsersAgainstThreshold(bot); }, waitMs);
+	}
 };
