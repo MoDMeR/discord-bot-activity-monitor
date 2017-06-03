e54612765c905517e82daee8fa37cd215be6f8ab
benji7425 <benji7425@outlook.com>
2017-06-03 22:40:55 +0100

    Add saving of last message date with file writing at interims

--- app/index.js
+++ app/index.js
@@ -5,17 +5,31 @@ var fs = require("fs");
 //external lib imports
 var JsonFile = require("jsonfile");
 
-var users;
+var users = {};
+var config;
 
 module.exports = (_config) => {
-	var config = _config || require("./config.json");
+	config = _config || require("./config.json");
 
 	this.onReady = (bot) => {
 		if (fs.existsSync(config.saveFile))
 			users = JsonFile.readFileSync(config.saveFile); //load any data we already have stored
+		Console.log(users);
+
+		writeToFile();
+	};
+
+	this.onMessage = (bot, user, userID, channelID, message) => {
+		users[userID] = new Date(); //save this message as the user's last active date
 		Console.log(users);
 	};
 
 	return this;
 };
 
+var writeToFile = () => {
+	JsonFile.writeFile(config.saveFile, users, (err) => { if (err) Console.error(err); });
+	// let saveIntervalMs = config.saveIntervalMins * 60 * 1000;
+	let saveIntervalMs = 1000;
+	setTimeout(writeToFile, saveIntervalMs);
+};
